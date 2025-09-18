ad96ffbd632053c9eea511caf8547ac7f7f4106b
Igor Tavares <igorviniciusliradasilva7@gmail.com>
2025-09-17 21:36:19 -0300

    Tratamento de Dados

--- Aulas/script/ex002.js
+++ Aulas/script/ex002.js
@@ -1,6 +1,8 @@
-3 + 2 
+/* var none = window.prompt ('Qual é seu nome?')
+window.alert ('Foi um prazer te conhecer '+ none + '!' ) */
 
-var igreja = 'João 3:16'
+var n1 = Number(window.prompt ('Qual seu número favorito?'))
+var n2 = Number(window.prompt ('Agora digite o número que você não gosta?'))
+var s = n1 + n2 
 
-igreja
-
+window.alert ('A soma dos valores é: ' + s)
