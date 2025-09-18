cd78596765c6ec3724fde453645db4a1cfce9e75
Igor Tavares <igorviniciusliradasilva7@gmail.com>
2025-09-17 22:59:14 -0300

    Tratamento de Dados

--- Aulas/script/ex002.js
+++ Aulas/script/ex002.js
@@ -1,8 +1,29 @@
 /* var none = window.prompt ('Qual é seu nome?')
 window.alert ('Foi um prazer te conhecer '+ none + '!' ) */
 
-var n1 = Number(window.prompt ('Qual seu número favorito?'))
+/* var n1 = Number(window.prompt ('Qual seu número favorito?'))
 var n2 = Number(window.prompt ('Agora digite o número que você não gosta?'))
 var s = n1 + n2 
 
-window.alert ('A soma dos valores é: ' + s)
+window.alert (`A soma entre os valores ${n1} e ${n2} e igual a: ${s}`) 
+
+var nome = window.prompt ('Qual é o seu nome?: ')
+var idade = window.prompt ('Qual é a sua idade?:')
+var nota = window.prompt ('Qual foi a sua nota?:')
+
+window.alert (`O aluno ${nome} que tem a idade de ${idade} tirou a nota de ${nota}`) */
+
+/*Localização de String: n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) */
+
+
+/*var n1 = 1531
+n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) 
+
+document.writeln (`Meu salario é de ${n1}`) */
+
+var nome = window.prompt ('Qual é seu nome:? ')
+window.alert (`Olá, ${nome}! Seu nome tem ${nome.length} letras`)
+
+document.writeln (`Olá, ${nome}!. Seu nome tem ${nome.length} letras <br\>`)  
+document.writeln (`Olá ${nome.toUpperCase()} esse é seu nome em maiúsculo <br\>`)
+document.writeln (`Ola ${nome.toLowerCase()} esse é seu nome minúsculo`)
